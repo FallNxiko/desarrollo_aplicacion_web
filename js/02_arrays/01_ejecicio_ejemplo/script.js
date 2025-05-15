603cb337ebe56e895536f9093bf4975c082be9cc
FallNxiko <nimajnebsoir@gmail.com>
2025-05-15 09:20:36 -0400

    awd

--- js/02_arrays/01_ejecicio_ejemplo/script.js
+++ js/02_arrays/01_ejecicio_ejemplo/script.js
@@ -1,4 +1,6 @@
+console.log("Conectado...")
+
 function mostrarDia() {
     let dias = ["lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo"];
-    alert("El segundo día de la semana es: " + dias[1]);
+    alert("El ultimo día de la semana es: " + dias[dias.length -1]);
 }
---
+++ js/02_arrays/Ejercicio_1_Promedio_de_valores/script.js
@@ -0,0 +1,7 @@
+console.log("hello_word...")
+
+function promedioTresPrimeros() {
+    let notas =[6.3, 7.0, 6.2, 5.3, 5.2 ]
+    let promedios =(notas [0]+ notas[1]+ notas[2])/3;
+    alert ("El promedio de los 3 primeros es : \n " + promedios)
+}
