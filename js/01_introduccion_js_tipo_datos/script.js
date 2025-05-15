ad5a1b266589fd1b6cad8de7dcce98f80024a3ba
FallNxiko <nimajnebsoir@gmail.com>
2025-05-15 10:58:46 -0400

    awd

--- js/01_introduccion_js_tipo_datos/script.js
+++ js/01_introduccion_js_tipo_datos/script.js
@@ -1,1 +1,71 @@
 console.log("conexion establecida con j!");
+
+let numeroEntero = 10
+let numeroDecimal = 3.14
+let cadenaTexto = "hello word "
+let valorBooleano = true
+let arregloNumeros = [1, 2, 3, 4, 5, 6,]
+let objetoPersona = "Benjamin"
+let valorIndefinido
+let valorNulo = null
+let identificadorUnico = Symbol
+let numeroGrande = BigInt
+const mes = "mayo"
+
+function mostrarNumeroEntero() {
+    console.log("Número Entero:", numeroEntero);
+}
+
+function mostrarnumeroDecimal() {
+    console.log("Numero decimal:", numeroDecimal);
+}
+
+
+function mostrarcadenaTexto() {
+    console.log("cadena de texto:", cadenaTexto);
+}
+
+function mostrarvalorBooleano() {
+    console.log("el valor booleano es:", valorBooleano);
+}
+
+function mostrararregloNumeros() {
+    console.log("los numeros son :", arregloNumeros);
+}
+
+function mostrarobjetoPersona() {
+    console.log("nombre de la persona es  :", objetoPersona);
+}
+
+function mostrarvalorIndefinido() {
+    console.log("no hay valor :", valorIndefinido);
+}
+
+function mostrarvalorNulo() {
+    console.log("el valor es :", valorNulo);
+}
+
+function mostraridentificadorUnico() {
+    console.log("identificador unico es :", identificadorUnico);
+}
+
+function mostrarnumeroGrande() {
+    console.log("numero grande es :", numeroGrande);
+}
+
+function operacion(){
+    return numeroDecimal + numeroEntero;
+}
+
+
+mostrarNumeroEntero()
+mostrarnumeroDecimal()
+mostrarcadenaTexto()
+mostrarvalorBooleano()
+mostrararregloNumeros()
+mostrarobjetoPersona()
+mostrarvalorIndefinido()
+mostrarvalorNulo()
+mostraridentificadorUnico()
+mostrarnumeroGrande()
+operacion()
