4cf19046106b1d76049fd78b51644952e8b1d5cd
FallNxiko <nimajnebsoir@gmail.com>
2025-05-15 09:57:46 -0400

    awd

---
+++ js/02_arrays/Ejercicio_2_Suma_de_extremos/script.js
@@ -0,0 +1,7 @@
+console.log("hello word")
+
+function sumaExtremos(){
+    let numeros =[6, 3, 7, 4, 6, 2, 5, 3, 5, 2 ]
+    let SUMAS =(numeros [0]+ numeros[numeros.length -1]);
+    alert ("La suma de los dos extremos es : \n " + SUMAS)
+}
---
+++ js/02_arrays/Ejercicio_3_Diferencia_entre_centro_y_extremos/script.js
@@ -0,0 +1,9 @@
+console.log("hello word")
+
+function diferenciaCentroExtremos(){
+    let numeros =[6, 3, 7, 4, 6, 2, 5, 3, 5, 2 ]
+    let extremos =(numeros [0]+ numeros[numeros.length -1]);
+    alert ("La suma de los dos extremos es : \n " + SUMAS)
+}
+
+// aun no esta echa
