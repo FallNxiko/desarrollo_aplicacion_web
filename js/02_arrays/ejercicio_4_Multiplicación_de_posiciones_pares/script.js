dda968bab14e3ba9036206c3d6c890b7a9838731
FallNxiko <nimajnebsoir@gmail.com>
2025-05-16 12:57:07 -0400

    awd

---
+++ js/02_arrays/ejercicio_4_Multiplicación_de_posiciones_pares/script.js
@@ -0,0 +1,7 @@
+console.log("hello word")
+
+function multiplicaPares(){
+    let numeros =[6, 3, 7, 4, 9, 2, 5, 3, 5, 2 ]
+    let pares =(numeros [0]* numeros[2]* numeros[4]);
+    alert ("La multiplicacion de los numeros es : \n " + pares)
+}
