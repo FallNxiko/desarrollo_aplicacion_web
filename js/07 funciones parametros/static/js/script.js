431d7f62007f414a423b03198027b5fa5e9c1de8
Dev_FallNxiko <nimajnebsoir@gmail.com>
2025-07-25 12:58:38 -0400

    a

--- js/07 funciones parametros/static/js/script.js
+++ js/07 funciones parametros/static/js/script.js
@@ -10,7 +10,6 @@ function RepetirPalabra(palabra, veces){
     }
     return resultado.trim();
 }
-
 function MostrarPalabra(){
     let input1 = document.getElementById("input1");
     let palabra = input1.value;
@@ -18,4 +17,33 @@ function MostrarPalabra(){
     let resultadoFinal = RepetirPalabra(palabra, numero)
 
     document.getElementById("resultado1").textContent =resultadoFinal
+}
+
+// 🟦 Ejercicio 2: Comparar dos números
+// 📌 Usa input para pedir dos números. Crea una función con dos parámetros que compare los valores y retorne un mensaje indicando cuál número es mayor, menor o si son iguales. Muestra el mensaje 
+
+function compararNumeros(num1, num2){
+    if(num1 > num2){
+        return`${num1} es mayor que ${num2}`;
+    } else if (num1 < num2){
+        return`${num1} es menor que ${num2}`;
+    }else{
+        return`${num1} es igual que ${num2}`;
+    }
+}
+
+function mostrarComparacion(){
+    let input1 = document.getElementById("numComp1");
+    let input2 = document.getElementById("numComp2");
+    let num1 = parseFloat(input1.value);
+    let num2 = parseFloat(input2.value);
+    let resultado = compararNumeros(num1, num2);
+    document.getElementById("resultado2").textContent = resultado;
+
+    if (isNaN(num1) || isNaN(num2)){
+        alert("valor invalido, igrese solo numeros");
+    }
+    input1.value = "";
+    input1.focus();
+    input2.value ="";
 }
