8b5a864a4a3f42d568448b853c964266708694b7
Dev_FallNxiko <nimajnebsoir@gmail.com>
2025-07-30 12:35:02 -0400

    Update script.js

--- js/012 Evaluación 29-07 - JS/static/js/script.js
+++ js/012 Evaluación 29-07 - JS/static/js/script.js
@@ -1,14 +1,20 @@
+// inicio de la funcion 
+
 function NumerosPar(Numero) {
-    let resultado = []
+    let resultado = [] //declarando un array vacio
+    // ciclo for que recorre los numeros del 1 al numero ingresado
+    // y verifica si son pares
     for (let i = 1; i <= Numero; i++) {
         if (i % 2 === 0) {
             resultado.push(i);
         }
         console.log(resultado)
     }
+    // retorna un dato si se cumple la condicion 
     return resultado
 }
 
+// fin de la funcion
 function mostrarNumerosPar() {
     let Numero = parseInt(prompt("Ingresa un Numero:"));
     let numeros = NumerosPar(Numero);
